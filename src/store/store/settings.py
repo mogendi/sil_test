456ba6ef90b9b5dc9670307883595ac57b90ce9b
mogendi <mogendi.mongare@gmail.com>
2024-06-06 11:25:17 +0300

    (fix): fix static files

--- src/store/store/settings.py
+++ src/store/store/settings.py
@@ -128,8 +128,10 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.0/howto/static-files/
-
+# https://docs.djangoproject.com/en/5.0/howto/static-files/+
+SITE_ROOT = os.path.dirname(os.path.realpath(__file__))
+STATIC_ROOT = os.path.join(SITE_ROOT, "static/")
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 STATIC_URL = "static/"
 
 # Default primary key field type
